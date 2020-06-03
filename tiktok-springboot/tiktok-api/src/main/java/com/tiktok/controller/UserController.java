e18e716a008db7e55821718cb8f46aa36a063e64
Yukinichi <boscosuen@outlook.com>
2020-06-02 22:04:59 -0700

    add upload profile photo controller

---
+++ tiktok-springboot/tiktok-api/src/main/java/com/tiktok/controller/UserController.java
@@ -0,0 +1,83 @@
+package com.tiktok.controller;
+
+import com.tiktok.pojo.Users;
+import com.tiktok.pojo.vo.UsersVO;
+import com.tiktok.service.UserService;
+import com.tiktok.utils.MD5Utils;
+import com.tiktok.utils.TiktokSONResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import tk.mybatis.mapper.util.StringUtil;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+@RestController
+@Api(value = "用户相关业务接口", tags = {"用户相关业务的controller"})
+@RequestMapping("/user")
+public class UserController extends BasicController {
+
+	@Autowired
+	private UserService userService;
+
+
+
+	@ApiOperation(value="用户上传头像", notes = "用户上传头像的接口")
+	@ApiImplicitParam(name = "userId", value="用户id", required = true, dataType = "String", paramType = "query")
+	@PostMapping("/uploadFace")
+	public TiktokSONResult login(String userId, @RequestParam("file") MultipartFile[] files) throws Exception {
+
+		// 文件保存的命名空间
+		String fileSpace = "/Users/suen/project/tiktok-dev";
+
+		//保存到数据库中的相对路径
+		String uploadPathDB = "/" + userId + "/face";
+		FileOutputStream fileOutputStream = null;
+		InputStream inputStream = null;
+
+		try {
+			if (files != null && files.length > 0) {
+
+
+				String fileName = files[0].getOriginalFilename();
+				if (StringUtils.isNotBlank(fileName)) {
+					// 文件上传的最终保存路径
+					String finalFacePath = fileSpace + uploadPathDB + "/" + fileName;
+					// 设置数据库保存的路径
+					uploadPathDB += ("/" + fileName);
+
+					File outFile = new File(finalFacePath);
+					if (outFile.getParentFile() != null || !outFile.getParentFile().isDirectory()) {
+						// 创建父文件夹
+						outFile.getParentFile().mkdirs();
+					}
+
+					fileOutputStream = new FileOutputStream(outFile);
+					inputStream = files[0].getInputStream();
+					// IOUtils: org.apache.common.io
+					IOUtils.copy(inputStream, fileOutputStream);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (fileOutputStream != null) {
+				fileOutputStream.flush();
+				fileOutputStream.close();
+			}
+		}
+
+		return TiktokSONResult.ok();
+	}
+	
+}
